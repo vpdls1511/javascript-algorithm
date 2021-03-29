0dff84ee0faffbc10971ff70887383b04955d173
vpdls1511 <vpdls1511@gmail.com>
2021-03-29 11:18:59 +0900

    baekjoon-2438

---
+++ string/2438.js
@@ -0,0 +1,20 @@
+const readline = require('readline')
+const rl = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+})
+
+const input = [];
+
+rl.on('line' , line => {
+    input.push(line.split(' '))
+    const inputData = input[0][0]
+    for ( let i=0 ; i<inputData ; i++){
+        let point = ''
+        for ( let j=0 ; j < i+1 ; j++){
+            point += '*'
+        }
+        console.log(point)
+    }
+    process.exit()
+})
