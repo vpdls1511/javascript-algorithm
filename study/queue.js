7a1253c2682f11a6ece206b5bd3dbe982b590dc0
sosae <sosae.cafe24app.com>
2021-06-04 21:39:07 +0900

    change front <-> rear

--- study/queue.js
+++ study/queue.js
@@ -16,17 +16,23 @@ const queue = () => {
                 console.log('isEmpty')
             }else{
                 for(let i = front ; i < rear ; i++){
-                    console.log(arr[i+1])
+                    console.log(`[${i+1}] = ${arr[i+1]}`)
                 }
             }
+            console.log(' ')
         }
     }
 }
 
 queue().printQueue()
-queue().enqueue('2')
 queue().enqueue('1')
+queue().enqueue('2')
 queue().printQueue()
 queue().enqueue('3')
+queue().enqueue('4')
+queue().enqueue('5')
+queue().dequeue()
+queue().dequeue()
+queue().dequeue()
 queue().dequeue()
 queue().printQueue()
