74361b8800fc2b55ac5c8db50eb1d1d3ec66a79b
vpdls1511 <vpdls1511@gmail.com>
2021-08-05 14:41:58 +0900

    loop_3

---
+++ study/basicSkills/loop_3.js
@@ -0,0 +1,27 @@
+let str = '';
+let loop = 5;
+
+console.log('1번')
+for(let i = 0 ; i < loop; i++){
+    for(let j = 1 ; j <= loop; j++){
+        str += j+(i*loop)+ ' '
+    }
+    console.log(str)
+    str = ''
+}
+console.log('2번')
+for(let i = 0 ; i < loop; i++){
+    if(i%2===0){
+        for(let j = 1 ; j <= loop; j++){
+            str += j+(i*loop)+ ' '
+        }
+    }else{
+        for(let j = loop ; j >= 1; j--){
+            str += j+(i*loop)+ ' '
+        }
+    }
+    console.log(str)
+    str = ''
+}
+
+
--- study/basicSkills/loop_7.js
+++ study/basicSkills/loop_7.js
@@ -1,15 +1,8 @@
 let str = ''
-
 for(let i = 0 ; i < 5 ; i++){
-    str=''
-    if(i%2 === 0){
-        for(let j = 1 ; j < 6 ; j++){
-            str+=' ' + (i * 5 + j)
-        }
-    }else{
-        for(let j = 6 ; j > 1 ; j--){
-            str+=' ' + (i * 5 + j - 1)
-        }
+    for(let j=1 ; j <= 5; j++){
+        str += j +(i*5) + ' '
     }
-    console.log(str)
+    console.log(str);
+    str = ''
 }
