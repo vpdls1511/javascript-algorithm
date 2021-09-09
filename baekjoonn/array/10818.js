e09023d809304b62b6fce96a4e13b49807bfe8f0
vpdls1511 <vpdls1511@gmail.com>
2021-09-10 08:33:26 +0900

    10818

---
+++ baekjoonn/array/10818.js
@@ -0,0 +1,25 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const input = []
+rl.on('line', function (line) {
+    input.push(line)
+}).on('close', function () {
+    solution(input)
+    process.exit();
+});
+
+function solution(line) {
+    const lineData = line.toString().split(',')
+    const arr = lineData[1].toString().split(' ').map(x => Number(x))
+    let max = arr[0] , min = arr[0];
+    for(let i = 0 ; i < Number(lineData[0]) ; i++){
+        max = max < arr[i] ? arr[i] : max;
+        min = min > arr[i] ? arr[i] : min;
+    }
+
+    console.log(`${min} ${max}`)
+    return 0;
+}
