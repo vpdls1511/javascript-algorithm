f96040bc56ec35f6e706ecb5eeccc24aa89ce11e
김남규 <ngyu@gimnamgyuui-MacBookPro.local>
2021-10-14 17:25:04 +0900

    reduce tab space 4 to 2

--- baekjoonn/for/10950.js
+++ baekjoonn/for/10950.js
@@ -1,23 +1,23 @@
-const readline = require('readline')
+const readline = require("readline");
 const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const solution = (line) => {
-    const data = line;
+const solution = line => {
+  const data = line;
 
-    for(let i = 0 ; i < parseInt(data[0]) ; i++){
-        let n = parseInt(data[i+1][0]);
-        let m = parseInt(data[i+1][2]);
-        console.log(n+m);
-    }
-}
+  for (let i = 0; i < parseInt(data[0]); i++) {
+    let n = parseInt(data[i + 1][0]);
+    let m = parseInt(data[i + 1][2]);
+    console.log(n + m);
+  }
+};
 
 const input = [];
-rl.on('line' , line => {
-    input.push(line)
-}).on('close', ()=>{
-    solution(input);
-    process.exit();
-})
+rl.on("line", line => {
+  input.push(line);
+}).on("close", () => {
+  solution(input);
+  process.exit();
+});
--- baekjoonn/for/2438.js
+++ baekjoonn/for/2438.js
@@ -1,20 +1,20 @@
-const readline = require('readline')
+const readline = require("readline");
 const rl = readline.createInterface({
-    input : process.stdin,
-    output : process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const input = [];
 
-rl.on('line' , line => {
-    input.push(line.split(' '))
-    const inputData = input[0][0]
-    for ( let i=0 ; i<inputData ; i++){
-        let point = ''
-        for ( let j=0 ; j < i+1 ; j++){
-            point += '*'
-        }
-        console.log(point)
+rl.on("line", line => {
+  input.push(line.split(" "));
+  const inputData = input[0][0];
+  for (let i = 0; i < inputData; i++) {
+    let point = "";
+    for (let j = 0; j < i + 1; j++) {
+      point += "*";
     }
-    process.exit()
-})
+    console.log(point);
+  }
+  process.exit();
+});
