20f574aef85cf1010f9ec42923c1889c72682c4d
김남규 <ngyu@gimnamgyuui-MacBookPro.local>
2021-10-14 17:24:48 +0900

    reduce tab space 4 to 2

--- baekjoonn/func/8958.js
+++ baekjoonn/func/8958.js
@@ -1,20 +1,23 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
+function solution(number) {
+  let num = number;
+  let cnt = 99;
+  if (num < 100) return num;
+  if (num >= 1000) num = 999;
 
-function solution(number){
-    let num = number
-    let cnt = 99
-    if(num < 100) return num;
-    if(num >= 1000) num = 999;
+  for (let i = 100; i <= num; i++) {
+    const x = parseInt(i / 100);
+    const y = parseInt((i / 10) % 10);
+    const z = parseInt(i % 10);
+    if (x - y === y - z) cnt++;
+  }
 
-    for(let i = 100 ; i <= num ; i++){
-        const x = parseInt(i / 100)
-        const y = parseInt((i / 10) % 10)
-        const z = parseInt(i % 10)
-        if(x-y === y-z) cnt++
-    }
-
-    return cnt
+  return cnt;
 }
 
-console.log(solution(parseInt(input[0])))
+console.log(solution(parseInt(input[0])));
