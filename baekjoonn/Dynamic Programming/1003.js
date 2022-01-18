9fe2da2293439f7616c679ab93e2c530b28bc711
vpdls1511 <vpdls1511@gmail.com>
2022-01-18 14:13:06 +0900

    1003

--- baekjoonn/Dynamic Programming/1003.js
+++ baekjoonn/Dynamic Programming/1003.js
@@ -4,6 +4,14 @@ let input = require("fs")
   .trim()
   .split("\n").map(v => parseInt(v));
 
-let arr = new Array( input[0] , 2 )
+let zeroCnt = [1 , 0 , 1]; // 0 나온 횟수
+let oneCnt = [0 , 1 , 1] // 1 나온 횟수
 
-console.log(arr)
+for(let i = 3 ; i <= 40 ; i++){
+  zeroCnt.push( zeroCnt[i-1] + zeroCnt[i-2] )
+  oneCnt.push( oneCnt[i-1] + oneCnt[i-2] )
+}
+
+for (let i = 1 ; i < input.length ; i++){
+  console.log(`${zeroCnt[input[i]]} ${oneCnt[input[i]]}`)
+}
